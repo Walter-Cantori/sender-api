80ea87b7a54870701364375e5610c7d3d4f31286
walter cantori <walter.cantori@gmail.com>
2021-03-01 22:42:07 -0300

    reduce bundle 3

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   mode: isLocal ? 'development' : 'production',
   entry: slsw.lib.entries,
   externals: [nodeExternals()],
-  devtool: isLocal ? 'source-map' : false,
+  devtool: isLocal ? 'eval-source-map' : 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
   },
