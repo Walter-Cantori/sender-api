9023fdbfffe89a626af777e349d58f29611f3b65
walter cantori <walter.cantori@gmail.com>
2021-03-02 21:42:44 -0300

    add upload lambda

---
+++ src/functions/projects/processProjectsUpload.ts
@@ -0,0 +1,46 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+
+const handler: APIGatewayProxyHandler = async (event) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({ hello: 'ok' }),
+  };
+
+  return response;
+};
+
+export { handler };
+
+// function setupDynamoDB() {
+//   if (process.env.IS_LOCAL) {
+//     const host = process.env.DB_HOST;
+//     const port = process.env.DB_PORT;
+//     return new DynamoDB.DocumentClient({
+//       region: host,
+//       accessKeyId: 'DEFAULT_ACCESS_KEY',
+//       secretAccessKey: 'DEFAULT_SECRET',
+//       endpoint: new Endpoint(`http://${host}:${port}`),
+//     });
+//   }
+//   return new DynamoDB.DocumentClient();
+// }
+// const dynamoDB = setupDynamoDB();
+
+//   const data = {
+//     TableName: process.env.DbTableName || '',
+//     Item: {
+//       id: v4(),
+//       createdAt: new Date().toISOString(),
+//       name: 'testname',
+//     },
+//   };
+//   console.log('writing data to db');
+
+//   await dynamoDB.put(data).promise();
+
+//   console.log('reading from db');
+//   const result = await dynamoDB
+//     .scan({
+//       TableName: 'projects',
+//     })
+//     .promise();
--- todo.md
+++ todo.md
@@ -1,14 +1,12 @@
 ## issues
 
-- function size too large, after adding webpack bundle reduced 40MB
-  check webpack bundler analyzer to investigate
-  https://nordcloud.com/minimizing-aws-lambda-deployment-package-size-in-typescript/
+- :)
 
 ## to do
 
 - create repository/service for one api
-- Setup debugging
 - authentication
+- create logging
 - create all APIs using api gateway
 - create sqs integration
 - create apis using sqs
